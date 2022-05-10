3d28bc3329d7d0d073b750e9245a3cb28a7000d6
Pinoyburn <deondre.garcia@yahoo.com>
2022-05-09 23:50:29 -0700

    minor navbar change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,18 +5,17 @@ import { Link } from 'react-router-dom';
 
 
 function Navbar() {
-  const[icon, setIcon] = useState(false);
+  const[isHome, setIsHome] = useState(true);
 
   const navOptions = {
     root: null,
     threshold: 1,
-    rootMargin: '0px 0px -150px 0px'
+    rootMargin: '-20px 0px -150px 0px'
   }
   const navObserver = new IntersectionObserver(function(entries, navObserver) {
     entries.forEach(entry => {
       if (!entry.isIntersecting) {
         document.querySelector('.navbar').classList.add('navbar-transition');
-        setIcon(true);
         navObserver.unobvserve(entry.target);
       }
   })
@@ -40,23 +39,24 @@ function Navbar() {
             <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to="/graduation">Graduation</Link>
+            <Link onClick={() => setIsHome(false)} to="/graduation">Graduation</Link>
           </li>
           <li>
-            <Link to="/portraits">Portraits</Link>
+            <Link onClick={() => setIsHome(false)} to="/portraits">Portraits</Link>
           </li>
           <li>
-            <Link to="/landscapes">Landscapes</Link>
+            <Link onClick={() => setIsHome(false)} to="/landscapes">Landscapes</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link onClick={() => setIsHome(false)} to="/contact">Contact</Link>
           </li>
           <li>
-            <Link to="/aboutme">About Me</Link>
+            <Link onClick={() => setIsHome(false)} to="/aboutme">About Me</Link>
           </li>
         </ul>
       </div>
-      <div className='nav-border' />
+      {/* nav border is a check for intersection observer for the navbar */}
+      <div className='nav-border' style={ isHome ? {top: '50%'} : {top: '10px'}}/>
     </>
   );
 }
